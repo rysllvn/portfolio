708ec1cd577282d59ea159f63f8c5bf5ad971693
Ryan Sullivan <mrsulliv@gmail.com>
2022-08-12 23:52:22 -0700

    Use app.js instead of main.js

--- static/js/app.js
+++ static/js/app.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
   particlesJS('particles-js', {
     "particles": {
       "number": {
-        "value": 30,
+        "value": 20,
         "density": {
           "enable": true,
           "value_area": 800
